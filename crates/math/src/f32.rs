d39b765af49ae9e9b0e71f9113a94ad8f6867027
psvri <psvrishabh@gmail.com>
2024-05-11 11:48:29 +0530

    enable test_f32_log

--- crates/math/src/f32.rs
+++ crates/math/src/f32.rs
@@ -172,10 +172,6 @@ mod tests {
     );
 
     test_unary_op_float!(
-        #[cfg_attr(
-            target_os = "windows",
-            ignore = "Not passing in CI but passes in local 🤔"
-        )]
         test_f32_log,
         Float32ArrayGPU,
         Float32ArrayGPU,
