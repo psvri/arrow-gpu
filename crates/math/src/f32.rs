4e97f797bf750b5e56f585d4fec6a2aef0e48ce6
psvri <psvrishabh@gmail.com>
2024-05-11 11:34:11 +0530

    enable test_f32_cbrt

--- crates/math/src/f32.rs
+++ crates/math/src/f32.rs
@@ -122,10 +122,6 @@ mod tests {
     );
 
     test_unary_op_float!(
-        #[cfg_attr(
-            target_os = "windows",
-            ignore = "Not passing in CI but passes in local 🤔"
-        )]
         test_f32_cbrt,
         Float32ArrayGPU,
         Float32ArrayGPU,
