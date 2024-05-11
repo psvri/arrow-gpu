4df990c1d08267d6ff9238487bbbdb6d2e6eeeea
psvri <psvrishabh@gmail.com>
2024-05-11 11:40:43 +0530

    enable test_f32_exp2

--- crates/math/src/f32.rs
+++ crates/math/src/f32.rs
@@ -158,10 +158,6 @@ mod tests {
     );
 
     test_unary_op_float!(
-        #[cfg_attr(
-            target_os = "windows",
-            ignore = "Not passing in CI but passes in local 🤔"
-        )]
         test_f32_exp2,
         Float32ArrayGPU,
         Float32ArrayGPU,
